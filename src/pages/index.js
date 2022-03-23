c30ad254116ddb8b600e1e1aca0b65c0de7b8a3e
Alexander Trampe <alexander.trampe@gmx.de>
2022-03-23 12:01:39 +0100

    First changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,7 @@ function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get started
-          </Link>
-        </div>
-      </div>
+      
     </header>
   );
 }
@@ -33,7 +23,6 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
